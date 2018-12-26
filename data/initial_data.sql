a2a6502e07621ba3ab0c00dcdee87af078ce384d
Devinda <devindakarunarathna@gmail.com>
2018-12-26 22:16:32 +0530

    Added interceptions

--- data/initial_data.sql
+++ data/initial_data.sql
@@ -1,11 +1,21 @@
 -- user role permission
 
 --  permissions
-INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (1, 'User login permission', 'ROLE_LOGIN', null);
-INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (2, 'User login permission', 'ROLE_MANAGE_APPLICATION', null);
-INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (3, 'User login permission', 'ROLE_MANAGE_SUBSCRIBER', null);
-INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (4, 'User login permission', 'ROLE_MANAGE_ROLE', null);
-INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (5, 'User login permission', 'ROLE_MANAGE_USER', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (1, '', 'ROLE_LOGIN', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (2, '', 'ROLE_MANAGE_SELLING', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (3, '', 'ROLE_MANAGE_BUYING', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (4, '', 'ROLE_MANAGE_PAYMENT', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (5, '', 'ROLE_MANAGE_EXPENSE', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (6, '', 'ROLE_MANAGE_CR', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (7, '', 'ROLE_MANAGE_BUYER', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (8, '', 'ROLE_MANAGE_SELLER', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (9, '', 'ROLE_MANAGE_ITEM', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (10, '', 'ROLE_MANAGE_EXPENSE_TYPE', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (11, '', 'ROLE_MANAGE_FREEZER', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (12, '', 'ROLE_MANAGE_REMOVAL', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (13, '', 'ROLE_MANAGE_REPORT', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (14, '', 'ROLE_MANAGE_ROLE', null);
+INSERT INTO system_permission (id, description, permission_name, depends_on_permission_id) VALUES (15, '', 'ROLE_MANAGE_USER', null);
 
 -- user role
 INSERT INTO system_user_role (id, name) VALUES (1, 'ADMIN');
